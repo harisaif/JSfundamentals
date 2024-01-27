a026f214eda32f1f360b772991bae034cedb2a9f
harisaif <haris.saif@gmail.com>
2024-01-27 10:37:50 +0800

    JS Fundamentals Part 3

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+function add7(x){
+    return x+7;
+}
 
-console.log(23+97);
+function multiply(x, y){
+    return x*y;
+}
+
+function capitalize(word){
+    return word[0].toUpperCase();
+}
+console.log(capitalize("computer"));
+
+function lastLetter(x){
+    let len = x.length;
+    return x[len-1].toUpperCase();
+}
+console.log(lastLetter("computer"));
